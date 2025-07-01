cd45d06ca187a4423f0985ae94eaaa9136789daf
Tyrellmoore <tyrell.moore@hgs.hiddengeniusproject.org>
2025-07-01 11:51:47 -0400

    Initial Commit

---
+++ UserAuth/UserAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAuthApp.swift
+//  UserAuth
+//
+//  Created by Tyrell Moore on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct UserAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
